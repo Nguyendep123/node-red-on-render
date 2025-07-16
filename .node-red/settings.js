ce3e65bfb35946720d2ba77b99165c6b9c749538
Nguyendep123 <thaox12@gmail.com>
2025-07-16 11:01:22 +0700

    Update settings.js

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -1,17 +1,1 @@
-module.exports = {
-    flowFile: 'flows.json', //  Flow JSON bạn đang dùng
-    uiPort: process.env.PORT || 1880,
 
-    // Để bảo mật nếu bạn muốn thêm sau
-   //adminAuth: null,
-
-    functionGlobalContext: {}, // Cho phép dùng biến toàn cục
-
-    logging: {
-        console: {
-            level: "info",
-            metrics: false,
-            audit: false
-        }
-    }
-};
