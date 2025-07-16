6da826832215544afd98135cd584685aa79911ff
Nguyendep123 <thaox12@gmail.com>
2025-07-16 11:07:47 +0700

    Update settings.js

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -1,1 +1,18 @@
+module.exports = {
+    flowFile: 'flows.json', // Flow JSON bạn đang dùng
+    uiPort: process.env.PORT || 1880,
+
+    // Để bảo mật nếu bạn muốn thêm sau
+    // adminAuth: null,
+
+    functionGlobalContext: {}, // Cho phép dùng biến toàn cục
+
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    }
+};
 
